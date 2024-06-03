f282e102be9822642d30df5fa9e71cb65375e9cc
Daniel <deabreu83@gmail.com>
2024-06-03 13:44:13 -0500

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+
+// Importamos defineConfig para definir configuraciones de Vite
+import { defineConfig } from 'vite';
+
+
+
+/**
+ * Aqui se pueden añadir plugins
+ * Aqui se puede canbiar cual va ser el directorio publico
+ * Aqui se puede hacer cualquier otro cambio
+ */
+export default defineConfig({
+    // Con base colocamos el repositorio root que sale en github
+    base: '/js-vite-blackjack/',
+    plugins: []
+});
